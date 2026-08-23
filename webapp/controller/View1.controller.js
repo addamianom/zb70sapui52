19291ff9f1e998c2adc685cbacb3e9021f28b058
Michele Addamiano <addamianom@gmail.com>
2026-08-23 21:48:50 +0000

    init1

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,6 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
     "use strict";
+    
 
     return Controller.extend("com.demo.zb70sapui52.controller.View1", {
         onInit() {
