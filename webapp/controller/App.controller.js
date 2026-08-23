650d78bcced2418a661e09bb757ea0cf78b0f518
Michele Addamiano <addamianom@gmail.com>
2026-08-23 17:14:34 +0000

    init

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.demo.zb70sapui52.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/demo/zb70sapui52/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
